759210055979f2add97baf80597ad86d71efc1c2
Parth Shah <1539772@my.brunel.ac.uk>
2017-08-21 20:55:54 +0100

    Initial commit

---
+++ app/src/test/java/com/prominergroup/nareen/cs2001_2016_17_reassessment_nareen1234/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.prominergroup.nareen.cs2001_2016_17_reassessment_nareen1234;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
